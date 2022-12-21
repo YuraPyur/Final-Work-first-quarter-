c8f11863f15d92b49545af08b31d43e7cb8614ca
YuraPyur <yupyur1991@mail.ru>
2022-12-21 21:25:43 +0300

    выполнение задачи

--- Program.cs
+++ Program.cs
@@ -8,15 +8,27 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-Console.WriteLine("Введите значения через запятую");
+Console.WriteLine("Введите значения через запятую: ");
 string x = Console.ReadLine();
 var array = x.Split(',').ToArray();
 int j = 0;
-for (int i = 0; i < array.Length; i++) 
+for (int i = 0; i < array.Length; i++)
 {
-    if (array[i].Length<=3)
+    if (array[i].Length <= 3)
     {
         j++;
-        Console.WriteLine(array[i]);
+    }
+}
+Console.WriteLine("Вывод элементов, длина которых меньше или равна 3: ");
+string[] array2 = new string[j];
+int e = 0;
+for (int i = 0, b = 0; i < array.Length; i++)
+{
+
+    if (array[i].Length <= 3)
+    {
+        array2[b] = array[i];
+        Console.Write(array2[b] + " ");
+        b++;
     }
 }
